483b80b5a2e3521f90c7aa85424e0a86962d4d53
Noah Campbell <noahcampbell@gmail.com>
2013-08-14 19:56:15 -0700

    adding mustRender flag to Page

--- page/page.go
+++ page/page.go
@@ -3,16 +3,24 @@ package page
 import (
 	"io"
 	"bufio"
+	"bytes"
 )
 
 type Page struct {
+	mustRender bool
 }
 
 func ReadFrom(r io.Reader) (page *Page, err error) {
+	page = new(Page)
+
 	reader := bufio.NewReader(r)
-	_, err = reader.Peek(1)
+	c, err := reader.Peek(1)
 	if err != nil {
 		return
+	}
+	
+	if !bytes.Equal(c, []byte("<")) {
+		page.mustRender = true
 	}
 	return
 }
--- page/page_test.go
+++ page/page_test.go
@@ -6,12 +6,51 @@ import (
 )
 
 var (
-	EMPTY_PAGE = ""
+	CONTENT_EMPTY = ""
+	CONTENT_NO_FRONTMATTER = "a page with no front matter"
+	CONTENT_WITH_FRONTMATTER = `---
+	front matter
+	---
+	Content with front matter`
+	CONTENT_HTML_NODOCTYPE = `<html><body></body></html>`
+	CONTENT_HTML_WITHDOCTYPE = `<!doctype html><html><body></body></html>`
+	CONTENT_HTML_WITH_FRONTMATTER = `---
+	front matter
+	---
+	<!doctype><html><body></body></html>`
 )
 
+func pageMust(page *Page, err error) *Page {
+	if err != nil {
+		panic(err)
+	}
+	return page
+}
+
 func TestCreatePageFrom(t *testing.T) {
-	_, err := ReadFrom(strings.NewReader(EMPTY_PAGE))
+	_, err := ReadFrom(strings.NewReader(CONTENT_EMPTY))
 	if err == nil {
 		t.Fatalf("Should not be able to read empty page.")
 	}
 }
+
+func TestStandaloneCreatePageFrom(t *testing.T) {
+	tests := []struct {
+		content string
+		expectedMustRender bool
+	}{
+		{CONTENT_NO_FRONTMATTER, true},
+		{CONTENT_WITH_FRONTMATTER, true},
+		{CONTENT_HTML_NODOCTYPE, false},
+		{CONTENT_HTML_WITHDOCTYPE, false},
+		{CONTENT_HTML_WITH_FRONTMATTER, true},
+	}
+	
+	for _, test := range tests {
+		p := pageMust(ReadFrom(strings.NewReader(test.content)))
+		if p.mustRender != test.expectedMustRender {
+			t.Fatalf("page.mustRender should be %t, got: %t", test.expectedMustRender, p.mustRender)
+		}
+	}
+}
+
