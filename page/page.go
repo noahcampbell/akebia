53e45f26a0dc6457a4549a22131841b3f60e6b10
Noah Campbell <noahcampbell@gmail.com>
2013-08-16 10:35:10 -0700

    Provide more testing when parsing a page.
    Includes supporting windows CRLF encoding.

--- page/page.go
+++ page/page.go
@@ -1,26 +1,115 @@
 package page
 
 import (
-	"io"
 	"bufio"
 	"bytes"
+	"errors"
+	"io"
+	"unicode"
+)
+
+const (
+	HTML_LEAD       = "<"
+	YAML_LEAD       = "-"
+	YAML_DELIM_UNIX = "---\n"
+	YAML_DELIM_DOS  = "---\r\n"
 )
 
 type Page struct {
-	mustRender bool
+	render      bool
+	frontmatter []byte
 }
 
-func ReadFrom(r io.Reader) (page *Page, err error) {
-	page = new(Page)
+type FrontMatter []byte
 
+func ReadFrom(r io.Reader) (page *Page, err error) {
 	reader := bufio.NewReader(r)
-	c, err := reader.Peek(1)
-	if err != nil {
+
+	if err = chompWhitespace(reader); err != nil {
 		return
 	}
-	
-	if !bytes.Equal(c, []byte("<")) {
-		page.mustRender = true
+
+	firstFive := make([]byte, 5)
+	if _, err = reader.Read(firstFive); err != nil {
+		return
+	}
+
+	page = new(Page)
+	page.render = shouldRender(firstFive)
+
+	if page.render && isFrontMatterDelim(firstFive) {
+		fm, err := extractFrontMatter(reader)
+		if err != nil {
+			return nil, err
+		}
+		page.frontmatter = fm
+	}
+
+	return
+}
+
+func chompWhitespace(r io.RuneScanner) (err error) {
+	for {
+		c, _, err := r.ReadRune()
+		if err != nil {
+			return err
+		}
+		if !unicode.IsSpace(c) {
+			r.UnreadRune()
+			return nil
+		}
 	}
 	return
+}
+
+func shouldRender(lead []byte) (frontmatter bool) {
+	if len(lead) <= 0 {
+		return
+	}
+
+	if bytes.Equal(lead[:1], []byte(HTML_LEAD)) {
+		return
+	}
+	return true
+}
+
+func isFrontMatterDelim(data []byte) bool {
+	if !bytes.Equal(data[:1], []byte(YAML_LEAD)) {
+		return false
+	}
+
+	if len(data) >= 4 && bytes.Equal(data[:4], []byte(YAML_DELIM_UNIX)) {
+		return true
+	}
+
+	if len(data) >= 5 && bytes.Equal(data[:5], []byte(YAML_DELIM_DOS)) {
+		return true
+	}
+
+	return false
+}
+
+// extractFrontMatter looks for the --- sequence followed by a newline
+func extractFrontMatter(r io.Reader) (fm FrontMatter, err error) {
+	buf := make([]byte, 1024) // TODO make this not a fixed buffer
+	if _, err = r.Read(buf); err != nil {
+		return nil, err
+	}
+
+	// strip off front matter delim if it's present.
+	if isFrontMatterDelim(buf) {
+		buf = buf[4:]
+	}
+
+	for i, c := range buf {
+		switch c {
+		case '\r':
+			continue
+		case '\n':
+			if isFrontMatterDelim(buf[i+1:]) {
+				return FrontMatter(buf[:i]), nil
+			}
+		}
+	}
+	return nil, errors.New("Could not find Front Matter.")
 }
--- page/page_test.go
+++ page/page_test.go
@@ -1,24 +1,29 @@
 package page
 
 import (
-	"testing"
+	"bytes"
+	"os"
+	"path/filepath"
 	"strings"
+	"testing"
 )
 
 var (
-	CONTENT_EMPTY = ""
-	CONTENT_NO_FRONTMATTER = "a page with no front matter"
-	CONTENT_WITH_FRONTMATTER = `---
-	front matter
-	---
-	Content with front matter`
-	CONTENT_HTML_NODOCTYPE = `<html><body></body></html>`
-	CONTENT_HTML_WITHDOCTYPE = `<!doctype html><html><body></body></html>`
-	CONTENT_HTML_WITH_FRONTMATTER = `---
-	front matter
-	---
-	<!doctype><html><body></body></html>`
+	CONTENT_EMPTY                   = ""
+	CONTENT_NO_FRONTMATTER          = "a page with no front matter"
+	CONTENT_WITH_FRONTMATTER        = "---\ntitle: front matter\n---\nContent with front matter"
+	CONTENT_HTML_NODOCTYPE          = "<html><body></body></html>"
+	CONTENT_HTML_WITHDOCTYPE        = "<!doctype html><html><body></body></html>"
+	CONTENT_HTML_WITH_FRONTMATTER   = "---\ntilte: front matter\n---\n<!doctype><html><body></body></html>"
+	CONTENT_LWS_HTML                = "    <html><body></body></html>"
+	CONTENT_LWS_LF_HTML             = "\n<html><body></body></html>"
+	CONTENT_INCOMPLETE_BEG_FM_DELIM = "--\ntitle: incomplete beg fm delim\n---\nincomplete frontmatter delim"
+	CONTENT_INCOMPLETE_END_FM_DELIM = "---\ntitle: incomplete end fm delim\n--\nincomplete frontmatter delim"
+	CONTENT_MISSING_END_FM_DELIM    = "---\ntitle: incomplete end fm delim\nincomplete frontmatter delim"
+	CONTENT_FM_NO_DOC               = "---\ntitle: no doc\n---"
 )
+
+var lineEndings = []string{"\n", "\r\n"}
 
 func pageMust(page *Page, err error) *Page {
 	if err != nil {
@@ -27,30 +32,174 @@ func pageMust(page *Page, err error) *Page {
 	return page
 }
 
-func TestCreatePageFrom(t *testing.T) {
-	_, err := ReadFrom(strings.NewReader(CONTENT_EMPTY))
-	if err == nil {
-		t.Fatalf("Should not be able to read empty page.")
+func TestDegenerateCreatePageFrom(t *testing.T) {
+	tests := []struct {
+		content string
+	}{
+		{CONTENT_EMPTY},
+		{CONTENT_MISSING_END_FM_DELIM},
+		{CONTENT_INCOMPLETE_END_FM_DELIM},
+		{CONTENT_FM_NO_DOC},
+		//{CONTENT_INCOMPLETE_BEG_FM_DELIM},
+	}
+
+	for _, test := range tests {
+		for _, ending := range lineEndings {
+			test.content = strings.Replace(test.content, "\n", ending, -1)
+			_, err := ReadFrom(strings.NewReader(test.content))
+			if err == nil {
+				t.Errorf("Content should return an err:\n%s\n", test.content)
+			}
+		}
+	}
+}
+
+func checkPageRender(t *testing.T, p *Page, expected bool) {
+	if p.render != expected {
+		t.Errorf("page.render should be %t, got: %t", expected, p.render)
+	}
+}
+
+func checkPageFrontMatterIsNil(t *testing.T, p *Page, content string, expected bool) {
+	if bool(p.frontmatter == nil) != expected {
+		t.Logf("\n%q\n", content)
+		t.Errorf("page.frontmatter == nil? %t, got %t", expected, p.frontmatter == nil)
 	}
 }
 
 func TestStandaloneCreatePageFrom(t *testing.T) {
 	tests := []struct {
-		content string
+		content            string
 		expectedMustRender bool
+		frontMatterIsNil   bool
 	}{
-		{CONTENT_NO_FRONTMATTER, true},
-		{CONTENT_WITH_FRONTMATTER, true},
-		{CONTENT_HTML_NODOCTYPE, false},
-		{CONTENT_HTML_WITHDOCTYPE, false},
-		{CONTENT_HTML_WITH_FRONTMATTER, true},
+		{CONTENT_NO_FRONTMATTER, true, true},
+		{CONTENT_WITH_FRONTMATTER, true, false},
+		{CONTENT_HTML_NODOCTYPE, false, true},
+		{CONTENT_HTML_WITHDOCTYPE, false, true},
+		{CONTENT_HTML_WITH_FRONTMATTER, true, false},
+		{CONTENT_LWS_HTML, false, true},
+		{CONTENT_LWS_LF_HTML, false, true},
 	}
-	
+
 	for _, test := range tests {
-		p := pageMust(ReadFrom(strings.NewReader(test.content)))
-		if p.mustRender != test.expectedMustRender {
-			t.Fatalf("page.mustRender should be %t, got: %t", test.expectedMustRender, p.mustRender)
+		for _, ending := range lineEndings {
+			test.content = strings.Replace(test.content, "\n", ending, -1)
+			p := pageMust(ReadFrom(strings.NewReader(test.content)))
+			checkPageRender(t, p, test.expectedMustRender)
+			checkPageFrontMatterIsNil(t, p, test.content, test.frontMatterIsNil)
 		}
 	}
 }
 
+func TestLongFormRender(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			t.Errorf("panic/recover %s\n", err)
+		}
+	}()
+
+	tests := []struct {
+		filename string
+	}{
+		{"long_text_test.md"},
+	}
+	for _, test := range tests {
+		path := filepath.FromSlash(test.filename)
+		f, err := os.Open(path)
+		if err != nil {
+			t.Fatalf("Unable to open %s: %s", path, err)
+		}
+
+		p := pageMust(ReadFrom(f))
+		checkPageFrontMatterIsNil(t, p, "[long file]", false)
+	}
+}
+
+func TestPageShouldRender(t *testing.T) {
+	tests := []struct {
+		content  []byte
+		expected bool
+	}{
+		{[]byte{}, false},
+		{[]byte{'<'}, false},
+		{[]byte{'-'}, true},
+		{[]byte("--"), true},
+		{[]byte("---"), true},
+		{[]byte("---\n"), true},
+		{[]byte{'a'}, true},
+	}
+
+	for _, test := range tests {
+		for _, ending := range lineEndings {
+			test.content = bytes.Replace(test.content, []byte("\n"), []byte(ending), -1)
+			if render := shouldRender(test.content); render != test.expected {
+
+				t.Errorf("Expected %s to shouldRender = %t, got: %t", test.content, test.expected, render)
+			}
+		}
+	}
+}
+
+func TestPageHasFrontMatter(t *testing.T) {
+	tests := []struct {
+		content  []byte
+		expected bool
+	}{
+		{[]byte{'-'}, false},
+		{[]byte("--"), false},
+		{[]byte("---"), false},
+		{[]byte("---\n"), true},
+		{[]byte("---\nA"), true},
+		// TODO support MAC encoding {[]byte("---\rA"), true},
+		{[]byte{'a'}, false},
+	}
+	for _, test := range tests {
+
+		for _, ending := range lineEndings {
+			test.content = bytes.Replace(test.content, []byte("\n"), []byte(ending), -1)
+			if isFrontMatterDelim := isFrontMatterDelim(test.content); isFrontMatterDelim != test.expected {
+				t.Errorf("Expected %q isFrontMatterDelim = %t,  got: %t", test.content, test.expected, isFrontMatterDelim)
+			}
+		}
+	}
+}
+
+func TestExtractFrontMatter(t *testing.T) {
+
+	tests := []struct {
+		frontmatter string
+		extracted   []byte
+		errIsNil    bool
+	}{
+		{"", nil, false},
+		{"-", nil, false},
+		{"---\n", nil, false},
+		{"---\nfoobar", nil, false},
+		{"---\nfoobar\nbarfoo\nfizbaz\n", nil, false},
+		{"---\nblar\n-\n", nil, false},
+		{"ralb\n---\n", []byte("ralb"), true},
+		{"minc\n---\ncontent", []byte("minc"), true},
+		{"cnim\n---\ncontent\n", []byte("cnim"), true},
+		{"---\nralb\n---\n", []byte("ralb"), true},
+		{"---\nminc\n---\ncontent", []byte("minc"), true},
+		{"---\ncnim\n---\ncontent\n", []byte("cnim"), true},
+	}
+
+	for _, test := range tests {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Errorf("panic/recovered: %s: %q", r, test.frontmatter)
+			}
+		}()
+		fm, err := extractFrontMatter(strings.NewReader(test.frontmatter))
+		if (err == nil) != test.errIsNil {
+			t.Logf("\n%q\n", string(test.frontmatter))
+			t.Errorf("Expected err == nil => %t, got: %t. err: %s", test.errIsNil, err == nil, err)
+			continue
+		}
+		if !bytes.Equal(fm, test.extracted) {
+			t.Errorf("Expected Front Matter %q. got %q", string(test.extracted), fm)
+		}
+	}
+}
